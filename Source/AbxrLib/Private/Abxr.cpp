55683746576ccdff10aa61f3597637766a9531b7
Nick Ramirez <nick.ramirez@arborxr.com>
2025-08-11 12:19:30 -0700

    Event wrappers

--- Source/AbxrLib/Private/Abxr.cpp
+++ Source/AbxrLib/Private/Abxr.cpp
@@ -4,6 +4,11 @@
 #include "LogBatcher.h"
 #include "TelemetryBatcher.h"
 
+TMap<FString, int64> UAbxr::AssessmentStartTimes;
+TMap<FString, int64> UAbxr::ObjectiveStartTimes;
+TMap<FString, int64> UAbxr::InteractionStartTimes;
+TMap<FString, int64> UAbxr::LevelStartTimes;
+
 void UAbxr::Authenticate()
 {
     Authentication::Authenticate();
@@ -39,7 +44,120 @@ void UAbxr::Event(const FString& Name, const TMap<FString, FString>& Meta)
 	EventBatcher::Add(Name, Meta);
 }
 
+/**
+* Add event information
+*
+* @param Name      Name of the event
+* @param Position  Adds position tracking of the object
+* @param Meta      Any additional information
+*/
+void UAbxr::Event(const FString& Name, const FVector& Position, TMap<FString, FString>& Meta)
+{
+    Meta.Add(TEXT("position_x"), FString::SanitizeFloat(Position.X));
+    Meta.Add(TEXT("position_y"), FString::SanitizeFloat(Position.Y));
+    Meta.Add(TEXT("position_z"), FString::SanitizeFloat(Position.Z));
+    Event(Name, Meta);
+}
+
+/**
+* Add telemetry information
+*
+* @param Name      Name of the telemetry
+* @param Meta      Any additional information
+*/
 void UAbxr::TelemetryEntry(const FString& Name, const TMap<FString, FString>& Meta)
 {
     TelemetryBatcher::Add(Name, Meta);
+}
+
+void UAbxr::EventAssessmentStart(const FString& AssessmentName, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("assessment"));
+	Meta.Add(TEXT("verb"), TEXT("started"));
+	AssessmentStartTimes.Add(AssessmentName, FDateTime::UtcNow().ToUnixTimestamp());
+	Event(AssessmentName, Meta);
+}
+
+void UAbxr::EventAssessmentComplete(const FString& AssessmentName, const int Score, EEventStatus Status, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("assessment"));
+	Meta.Add(TEXT("verb"), TEXT("completed"));
+	Meta.Add(TEXT("score"), FString::FromInt(Score));
+	Meta.Add(TEXT("status"), StaticEnum<EEventStatus>()->GetNameStringByValue(static_cast<int64>(Status)));
+	AddDuration(AssessmentStartTimes, AssessmentName, Meta);
+	Event(AssessmentName, Meta);
+	EventBatcher::Send();
+}
+
+void UAbxr::EventObjectiveStart(const FString& ObjectiveName, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("objective"));
+	Meta.Add(TEXT("verb"), TEXT("started"));
+	ObjectiveStartTimes.Add(ObjectiveName, FDateTime::UtcNow().ToUnixTimestamp());
+	Event(ObjectiveName, Meta);
+}
+
+void UAbxr::EventObjectiveComplete(const FString& ObjectiveName, const int Score, EEventStatus Status, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("objective"));
+	Meta.Add(TEXT("verb"), TEXT("completed"));
+	Meta.Add(TEXT("score"), FString::FromInt(Score));
+	Meta.Add(TEXT("status"), StaticEnum<EEventStatus>()->GetNameStringByValue(static_cast<int64>(Status)));
+	AddDuration(ObjectiveStartTimes, ObjectiveName, Meta);
+	Event(ObjectiveName, Meta);
+}
+
+void UAbxr::EventInteractionStart(const FString& InteractionName, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("interaction"));
+	Meta.Add(TEXT("verb"), TEXT("started"));
+	InteractionStartTimes.Add(InteractionName, FDateTime::UtcNow().ToUnixTimestamp());
+	Event(InteractionName, Meta);
+}
+
+void UAbxr::EventInteractionComplete(const FString& InteractionName, EInteractionType InteractionType, const FString& Response, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("type"), TEXT("interaction"));
+	Meta.Add(TEXT("verb"), TEXT("completed"));
+	Meta.Add(TEXT("interaction"), StaticEnum<EInteractionType>()->GetNameStringByValue(static_cast<int64>(InteractionType)));
+	if (!Response.IsEmpty()) Meta.Add(TEXT("response"), Response);
+	AddDuration(InteractionStartTimes, InteractionName, Meta);
+	Event(InteractionName, Meta);
+}
+
+void UAbxr::EventLevelStart(const FString& LevelName, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("verb"), TEXT("started"));
+	Meta.Add(TEXT("id"), LevelName);
+	LevelStartTimes.Add(LevelName, FDateTime::UtcNow().ToUnixTimestamp());
+	Event(TEXT("level_start"), Meta);
+}
+
+void UAbxr::EventLevelComplete(const FString& LevelName, const int Score, TMap<FString, FString>& Meta)
+{
+	Meta.Add(TEXT("verb"), TEXT("completed"));
+	Meta.Add(TEXT("id"), LevelName);
+	Meta.Add(TEXT("score"), FString::FromInt(Score));
+	AddDuration(LevelStartTimes, LevelName, Meta);
+	Event(LevelName, Meta);
+}
+
+void UAbxr::EventCritical(const FString& Label, const TMap<FString, FString>& Meta)
+{
+	const FString taggedName = TEXT("CRITICAL_ABXR_") + Label;
+	Event(taggedName, Meta);
+}
+
+void UAbxr::AddDuration(TMap<FString, int64>& StartTimes, const FString& Name, TMap<FString, FString>& Meta)
+{
+	if (StartTimes.Contains(Name))
+	{
+		const int64 Duration = FDateTime::UtcNow().ToUnixTimestamp() - StartTimes[Name];
+		Meta.Add(TEXT("duration"), FString::Printf(TEXT("%lld"), Duration));
+		StartTimes.Remove(Name);
+	}
+	else
+	{
+		Meta.Add(TEXT("duration"), TEXT("0"));
+	}
 }
--- Source/AbxrLib/Public/Abxr.h
+++ Source/AbxrLib/Public/Abxr.h
@@ -4,6 +4,29 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Abxr.generated.h"
 
+UENUM(BlueprintType)
+enum class EEventStatus : uint8
+{
+	Pass        UMETA(DisplayName = "pass"),
+	Fail        UMETA(DisplayName = "fail"),
+	Complete    UMETA(DisplayName = "complete"),
+	Incomplete  UMETA(DisplayName = "incomplete"),
+	Browsed     UMETA(DisplayName = "browsed")
+};
+
+UENUM(BlueprintType)
+enum class EInteractionType : uint8
+{
+	Bool         UMETA(DisplayName = "bool"),
+	Select       UMETA(DisplayName = "select"),
+	Text         UMETA(DisplayName = "text"),
+	Rating       UMETA(DisplayName = "rating"),
+	Number       UMETA(DisplayName = "number"),
+	Matching     UMETA(DisplayName = "matching"),
+	Performance  UMETA(DisplayName = "performance"),
+	Sequencing   UMETA(DisplayName = "sequencing")
+};
+
 UCLASS()
 class ABXRLIB_API UAbxr : public UBlueprintFunctionLibrary
 {
@@ -15,29 +38,111 @@ public:
 	
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void LogDebug(const FString& Text, const TMap<FString, FString>& Meta);
-	static void LogDebug(const FString& text) { LogDebug(text, TMap<FString, FString>()); }
+	static void LogDebug(const FString& Text) { LogDebug(Text, TMap<FString, FString>()); }
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void LogInfo(const FString& Text, const TMap<FString, FString>& Meta);
-	static void LogInfo(const FString& text) { LogInfo(text, TMap<FString, FString>()); }
+	static void LogInfo(const FString& Text) { LogInfo(Text, TMap<FString, FString>()); }
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void LogWarn(const FString& Text, const TMap<FString, FString>& Meta);
-	static void LogWarn(const FString& text) { LogWarn(text, TMap<FString, FString>()); }
+	static void LogWarn(const FString& Text) { LogWarn(Text, TMap<FString, FString>()); }
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void LogError(const FString& Text, const TMap<FString, FString>& Meta);
-	static void LogError(const FString& text) { LogError(text, TMap<FString, FString>()); }
+	static void LogError(const FString& Text) { LogError(Text, TMap<FString, FString>()); }
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void LogCritical(const FString& Text, const TMap<FString, FString>& Meta);
-	static void LogCritical(const FString& text) { LogCritical(text, TMap<FString, FString>()); }
+	static void LogCritical(const FString& Text) { LogCritical(Text, TMap<FString, FString>()); }
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void Event(const FString& Name, const TMap<FString, FString>& Meta);
-	static void Event(const FString& name) { Event(name, TMap<FString, FString>()); }
+	static void Event(const FString& Name) { Event(Name, TMap<FString, FString>()); }
+	static void Event(const FString& Name, const FVector& Position, TMap<FString, FString>& Meta);
+	static void Event(const FString& Name, const FVector& Position)
+	{
+		TMap<FString, FString> Meta;
+		Event(Name, Position, Meta);
+	}
 
 	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
 	static void TelemetryEntry(const FString& Name, const TMap<FString, FString>& Meta);
-	static void TelemetryEntry(const FString& name) { TelemetryEntry(name, TMap<FString, FString>()); }
+	static void TelemetryEntry(const FString& Name) { TelemetryEntry(Name, TMap<FString, FString>()); }
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventAssessmentStart(const FString& AssessmentName, TMap<FString, FString>& Meta);
+	static void EventAssessmentStart(const FString& AssessmentName)
+	{
+		TMap<FString, FString> Meta;
+		EventAssessmentStart(AssessmentName, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventAssessmentComplete(const FString& AssessmentName, const int Score, EEventStatus Status, TMap<FString, FString>& Meta);
+	static void EventAssessmentComplete(const FString& AssessmentName, const int Score, const EEventStatus Status)
+	{
+		TMap<FString, FString> Meta;
+		EventAssessmentComplete(AssessmentName, Score, Status, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventObjectiveStart(const FString& ObjectiveName, TMap<FString, FString>& Meta);
+	static void EventObjectiveStart(const FString& ObjectiveName)
+	{
+		TMap<FString, FString> Meta;
+		EventObjectiveStart(ObjectiveName, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventObjectiveComplete(const FString& ObjectiveName, const int Score, EEventStatus Status, TMap<FString, FString>& Meta);
+	static void EventObjectiveComplete(const FString& ObjectiveName, const int Score, const EEventStatus Status)
+	{
+		TMap<FString, FString> Meta;
+		EventObjectiveComplete(ObjectiveName, Score, Status, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventInteractionStart(const FString& InteractionName, TMap<FString, FString>& Meta);
+	static void EventInteractionStart(const FString& InteractionName)
+	{
+		TMap<FString, FString> Meta;
+		EventInteractionStart(InteractionName, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventInteractionComplete(const FString& InteractionName, EInteractionType InteractionType, const FString& Response, TMap<FString, FString>& Meta);
+	static void EventInteractionComplete(const FString& InteractionName, EInteractionType InteractionType, const FString& Response)
+	{
+		TMap<FString, FString> Meta;
+		EventInteractionComplete(InteractionName, InteractionType, Response, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventLevelStart(const FString& LevelName, TMap<FString, FString>& Meta);
+	static void EventLevelStart(const FString& LevelName)
+	{
+		TMap<FString, FString> Meta;
+		EventLevelStart(LevelName, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventLevelComplete(const FString& LevelName, const int Score, TMap<FString, FString>& Meta);
+	static void EventLevelComplete(const FString& LevelName, const int Score)
+	{
+		TMap<FString, FString> Meta;
+		EventLevelComplete(LevelName, Score, Meta);
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "AbxrLib")
+	static void EventCritical(const FString& Label, const TMap<FString, FString>& Meta);
+	static void EventCritical(const FString& Label) { EventCritical(Label, TMap<FString, FString>()); }
+
+private:
+	static TMap<FString, int64> AssessmentStartTimes;
+	static TMap<FString, int64> ObjectiveStartTimes;
+	static TMap<FString, int64> InteractionStartTimes;
+	static TMap<FString, int64> LevelStartTimes;
+
+	static void AddDuration(TMap<FString, int64>& StartTimes, const FString& Name, TMap<FString, FString>& Meta);
 };
