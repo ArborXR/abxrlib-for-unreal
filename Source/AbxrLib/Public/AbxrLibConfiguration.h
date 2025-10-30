3a812bcf16c3c694b868a69f26cd510412382b1b
Nick Ramirez <nick.ramirez@arborxr.com>
2025-10-30 16:34:43 -0700

    Add to config to start to match Unity config

--- Source/AbxrLib/Public/AbxrLibConfiguration.h
+++ Source/AbxrLib/Public/AbxrLibConfiguration.h
@@ -12,6 +12,10 @@ public:
 	UAbxrLibConfiguration()
 	{
 		RestUrl = TEXT("https://lib-backend.xrdm.app/");
+		EnableAutomaticTelemetry = true;
+		HeadsetControllerTracking = true;
+		PositionCapturePeriod = 1;
+		EnableSceneEvents = true;
 		FrameRateTrackingPeriodSeconds = 0.5f;
 		TelemetryTrackingPeriodSeconds = 10;
 		SendRetriesOnFailure = 3;
@@ -31,71 +35,87 @@ public:
 	virtual FName GetCategoryName() const override { return TEXT("Plugins"); }
 	virtual FName GetSectionName() const override { return TEXT("AbxrLib"); }
 	
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Auth", meta=(DisplayName="Application ID (required)"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Authentication", meta=(DisplayName="Application ID (required)"))
 	FString AppId;
 	void SetAppId(const FString& NewAppId) {this->AppId = NewAppId;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Auth", meta=(DisplayName="Organization ID (optional)"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Authentication", meta=(DisplayName="Organization ID (optional)"))
 	FString OrgId;
 	void SetOrgId(const FString& NewOrgId) {this->OrgId = NewOrgId;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Auth", meta=(DisplayName="Authorization Secret (optional)"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Authentication", meta=(DisplayName="Authorization Secret (optional)"))
 	FString AuthSecret;
 	void SetAuthSecret(const FString& NewAuthSecret) {this->AuthSecret = NewAuthSecret;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network", meta=(DisplayName="REST URL"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Service Provider", meta=(DisplayName="REST URL"))
 	FString RestUrl;
 	void SetRestUrl(const FString& NewRestUrl) {this->RestUrl = NewRestUrl;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Telemetry Tracking Period (seconds)"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Player Tracking", meta=(DisplayName="Enable Automatic Telemetry"))
+	bool EnableAutomaticTelemetry;
+	void SetEnableAutomaticTelemetry(const bool NewEnableAutomaticTelemetry) {this->EnableAutomaticTelemetry = NewEnableAutomaticTelemetry;}
+
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Player Tracking", meta=(DisplayName="Headset/Controller Tracking"))
+	bool HeadsetControllerTracking;
+	void SetHeadsetControllerTracking(const bool NewHeadsetControllerTracking) {this->HeadsetControllerTracking = NewHeadsetControllerTracking;}
+
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Player Tracking", meta=(DisplayName="Position Capture Period (seconds)"))
+	int PositionCapturePeriod;
+	void SetPositionCapturePeriod(const int NewPositionCapturePeriod) {this->PositionCapturePeriod = NewPositionCapturePeriod;}
+
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Player Tracking", meta=(DisplayName="Enable Scene Events"))
+	bool EnableSceneEvents;
+	void SetEnableSceneEvents(const bool NewEnableSceneEvents) {this->EnableSceneEvents = NewEnableSceneEvents;}
+
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Telemetry Tracking Period (seconds)"))
 	double TelemetryTrackingPeriodSeconds;
 	void SetTelemetryTrackingPeriodSeconds(const double NewTelemetryTrackingPeriodSeconds) {this->TelemetryTrackingPeriodSeconds = NewTelemetryTrackingPeriodSeconds;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Frame Rate Tracking Period (seconds)"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Frame Rate Tracking Period (seconds)"))
 	double FrameRateTrackingPeriodSeconds;
 	void SetFrameRateTrackingPeriodSeconds(const double NewFrameRateTrackingPeriodSeconds) {this->FrameRateTrackingPeriodSeconds = NewFrameRateTrackingPeriodSeconds;}
 	
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Send Retries on Failure"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Send Retries on Failure"))
 	int SendRetriesOnFailure;
 	void SetSendRetriesOnFailure(const int NewSendRetriesOnFailure) {this->SendRetriesOnFailure = NewSendRetriesOnFailure;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Send Retry Interval Seconds"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Send Retry Interval Seconds"))
 	int SendRetryIntervalSeconds;
 	void SetSendRetryIntervalSeconds(const int NewSendRetryIntervalSeconds) {this->SendRetryIntervalSeconds = NewSendRetryIntervalSeconds;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Send Next Batch Wait Seconds"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Send Next Batch Wait Seconds"))
 	int SendNextBatchWaitSeconds;
 	void SetSendNextBatchWaitSeconds(const int NewSendNextBatchWaitSeconds) {this->SendNextBatchWaitSeconds = NewSendNextBatchWaitSeconds;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Straggler Timeout Seconds"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Straggler Timeout Seconds"))
 	int StragglerTimeoutSeconds;
 	void SetStragglerTimeoutSeconds(const int NewStragglerTimeoutSeconds) {this->StragglerTimeoutSeconds = NewStragglerTimeoutSeconds;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Data Entries Per Send Attempt"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Data Entries Per Send Attempt"))
 	int DataEntriesPerSendAttempt;
 	void SetDataEntriesPerSendAttempt(const int NewDataEntriesPerSendAttempt) {this->DataEntriesPerSendAttempt = NewDataEntriesPerSendAttempt;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Storage Entries Per Send Attempt"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Storage Entries Per Send Attempt"))
 	int StorageEntriesPerSendAttempt;
 	void SetStorageEntriesPerSendAttempt(const int NewStorageEntriesPerSendAttempt) {this->StorageEntriesPerSendAttempt = NewStorageEntriesPerSendAttempt;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Prune Sent Items Older Than Hours"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Prune Sent Items Older Than Hours"))
 	int PruneSentItemsOlderThanHours;
 	void SetPruneSentItemsOlderThanHours(const int NewPruneSentItemsOlderThanHours) {this->PruneSentItemsOlderThanHours = NewPruneSentItemsOlderThanHours;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Maximum Cached Items"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Maximum Cached Items"))
 	int MaximumCachedItems;
 	void SetMaximumCachedItems(const int NewMaximumCachedItems) {this->MaximumCachedItems = NewMaximumCachedItems;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Retain Local After Sent"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Retain Local After Sent"))
 	bool RetainLocalAfterSent;
 	void SetRetainLocalAfterSent(const bool NewRetainLocalAfterSent) {this->RetainLocalAfterSent = NewRetainLocalAfterSent;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Disable Automatic Telemetry"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Disable Automatic Telemetry"))
 	bool DisableAutomaticTelemetry;
 	void SetDisableAutomaticTelemetry(const bool NewDisableAutomaticTelemetry) {this->DisableAutomaticTelemetry = NewDisableAutomaticTelemetry;}
 
-	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Data Sending Rules", meta=(DisplayName="Disable Scene Events"))
+	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, Category="Network Configuration", meta=(DisplayName="Disable Scene Events"))
 	bool DisableSceneEvents;
 	void SetDisableSceneEvents(const bool NewDisableSceneEvents) {this->DisableSceneEvents = NewDisableSceneEvents;}
 };
