a7008e42301767f7ed89749b3df7faa4ff49888f
Nick Ramirez <nick.ramirez@arborxr.com>
2026-03-18 15:49:18 -0700

    Fix build error for older Android API versions

--- Source/AbxrLib/Private/Android/Java/com/abxr/lib/AbxrAndroidPackageUtils.java
+++ Source/AbxrLib/Private/Android/Java/com/abxr/lib/AbxrAndroidPackageUtils.java
@@ -6,25 +6,15 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 
 public class AbxrAndroidPackageUtils {
+    @SuppressWarnings("deprecation")
     public static boolean isPackageInstalled(String packageName) {
         try {
             Activity activity = com.epicgames.unreal.GameActivity.Get();
-            if (activity == null) {
-                return false;
-            }
+            if (activity == null) return false;
 
             PackageManager pm = activity.getPackageManager();
-
-            if (Build.VERSION.SDK_INT >= 33) {
-                PackageInfo info = pm.getPackageInfo(
-                    packageName,
-                    PackageManager.PackageInfoFlags.of(0)
-                );
-                return info != null;
-            } else {
-                PackageInfo info = pm.getPackageInfo(packageName, 0);
-                return info != null;
-            }
+            PackageInfo info = pm.getPackageInfo(packageName, 0);
+            return info != null;
         } catch (Exception e) {
             return false;
         }
